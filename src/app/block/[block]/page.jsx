eb08b13edbb8e79d3ff5bf08101e1db957d76941
xhufg <minardi.alessio99@gmail.com>
2024-01-02 18:12:51 +0100

    added block and tx info

--- src/app/block/[block]/page.jsx
+++ src/app/block/[block]/page.jsx
@@ -1,6 +1,14 @@
 "use client";
 import Navbar from "@/components/navbar";
-import { useState } from "react";
+import { useState, useEffect} from "react";
+import { Alchemy, Network } from "alchemy-sdk";
+
+const settings = {
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
 
 export default function Page({ params }) {
   const [selectedItem, setSelectedItem] = useState("block");
@@ -8,12 +16,58 @@ export default function Page({ params }) {
 const handleItemClick = (item) => {
   setSelectedItem(item);
 };
-  return(
+  const [blockDetails, setBlockDetails] = useState(null);
+
+  useEffect(() => {
+    async function getBlockDetails() {
+      try {
+        const block = await alchemy.core.getBlockWithTransactions(
+          parseInt(params.block)
+        );
+        setBlockDetails(block);
+        console.log(block)
+      } catch (error) {
+        console.error("Error fetching block details:", error);
+      }
+    }
+
+    if (params.block) {
+      getBlockDetails();
+    }
+  }, [params.block]);
+
+  if (!blockDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
     <>
-   <Navbar selectedItem={selectedItem} handleItemClick={handleItemClick} />
-    <div>
-      <h1>{params.block}</h1>
-    </div>
+    <Navbar selectedItem={selectedItem} handleItemClick={handleItemClick} />
+      <div className="flex justify-center mt-12">
+        <div className=" w-2/3 rounded p-4 border border-black-400 rounded-md shadow-md">
+          <div>
+            <div className="flex items-center ">
+              <span className="text-4xl">Block Details</span>
+            </div>
+
+            <hr />
+            <div>
+              <p>Block Number: {blockDetails.number}</p>
+              <p>Block Hash: {blockDetails.hash}</p>
+              <p>Miner : {blockDetails.miner} </p>
+              <p>Timestamp: {new Date(blockDetails.timestamp * 1000).toLocaleString()} </p>
+              <p>Transactions: {blockDetails.transactions.length}</p>
+              <p>Parent Hash: {blockDetails.parentHash}</p>
+  
+              
+              {/* Aggiungi altri dettagli del blocco come desiderato */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Altri dettagli del blocco come desiderato */}
     </>
-  )
-}
+  );
+};
+
--- src/app/tx/[tx]/page.jsx
+++ src/app/tx/[tx]/page.jsx
@@ -1,17 +1,79 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "@/components/navbar";
+import { Alchemy, Network } from "alchemy-sdk";
+import { formatEther, formatUnits,} from "ethers" 
+
+const settings = {
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
 
 export default function Page({ params }) {
   const [selectedItem, setSelectedItem] = useState("transactions");
+  const [receipt, setReceipt] = useState();
+  const [tx, setTx] = useState()
 
   const handleItemClick = (item) => {
     setSelectedItem(item);
   };
+
+  useEffect(() => {
+    async function getReceipt() {
+      try {
+        const rc = await alchemy.core.getTransactionReceipt(params.tx);
+        const txx = await alchemy.core.getTransaction(params.tx);
+        setTx(txx)
+        setReceipt(rc);
+       
+      } catch (error) {
+        console.error("Error fetching block details:", error);
+      }
+    }
+
+    if (params.tx) {
+      getReceipt();
+    }
+  }, [params.tx]);
+
+  if (!receipt) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <Navbar selectedItem={selectedItem} handleItemClick={handleItemClick} />
-      <div>My Post: {params.tx}</div>
+      <div className="flex justify-center mt-12">
+        <div className=" w-2/3 rounded p-4 border border-black-400 rounded-md shadow-md">
+          <div>
+            <div className="flex items-center ">
+              <span className="text-4xl">Transactions Details</span>
+            </div>
+
+            <hr />
+            <div>
+              <p>Tx Hash : {receipt.transactionHash}</p>
+              <p>Status : {receipt.status == 1 ? <span className="text-green-700 "> Success</span> : <span className="text-red-500 ">Failed</span>}</p>
+              
+              <p>From: {receipt.from}</p>
+              <p>To: {receipt.to}</p>
+              <p>Block : {receipt.blockNumber}</p>
+              <p>Confirmation : {receipt.confirmations.toString() }</p>
+              <p> Value : {parseFloat(formatEther(tx.value._hex)) } ETH</p>
+              <p>Tx Fee : {parseFloat(formatEther(tx.gasPrice._hex) * receipt.gasUsed.toString()) } ETH</p>
+              <p>Gas Price : {parseFloat(formatUnits(tx.gasPrice._hex, 'gwei')) } GWEI</p>
+              <p>Gas Used : {receipt.gasUsed.toString()}</p>
+              
+              <p></p>
+              {/* Aggiungi altri dettagli del blocco come desiderato */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Altri dettagli del blocco come desiderato */}
     </>
   );
 }
